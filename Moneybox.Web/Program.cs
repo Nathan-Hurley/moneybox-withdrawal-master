8d5d4bcff925314d6e0727ccc083bcbc1e7a15f0
Nathan Hurley <nath40@hotmail.co.uk>
2020-08-15 16:12:26 +0100

    Added Console Application to the project for testing

---
+++ Moneybox.Web/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Moneybox.App.DataAccess;
+using Moneybox.App.Domain.Services;
+using Moneybox.App.Features;
+
+namespace Moneybox.Web
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
